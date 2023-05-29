a20a076dee1998757d683ee2264ecfcad062b175
Brian Vaughn <brian.david.vaughn@gmail.com>
2023-05-29 17:13:12 -0400

    Fix Jest config to load JS esmodules

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,15 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  globals: {
+    "ts-jest": {
+      tsconfig: {
+        allowJs: true,
+      },
+    },
+  },
   preset: "ts-jest",
   testMatch: ["**/*.test.{ts,tsx}"],
+  transform: {
+    "^.+\\.[tj]sx?$": "ts-jest",
+  },
 };
